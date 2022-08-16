324b650ba05f33b296f25a27a8767b499a07ebc3
CodePellet <50582944+CodePellet@users.noreply.github.com>
2022-08-16 16:21:07 +0200

    Updated settings BrowserView
    Now is modal and only shows, when it is ready to show

--- src/views/view-settings.ts
+++ src/views/view-settings.ts
@@ -16,9 +16,14 @@ export class SettingsView {
             title: "Settings",
             icon: path.join(app.getAppPath(), "assets", "icons", "win", "icon.ico"),
             autoHideMenuBar: true,
-            modal: true
+            modal: true,
+            webPreferences: {
+                preload: path.join(__dirname, "..", "preload.js")
+            }
         });
         this.settingsWindow.loadFile(path.join(__dirname, "..", "..", "public", "views", "settings.html"));
-        this.settingsWindow.show();
+        this.settingsWindow.on("ready-to-show", () => {
+            this.settingsWindow.show();
+        });
     }
 }
